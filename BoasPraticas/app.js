6d7f02d45ddbae51e9af9ecd2b4f16cf948f9f94
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-05-24 22:12:18 -0300

    Quebrando em mais arquivos de rota, assim quando precisar adicionar uma rota nova, será acrescentado a nova rota e adicionado somente no arquivo index.js para ser carregado o novo arquivo de rota.

--- BoasPraticas/app.js
+++ BoasPraticas/app.js
@@ -1,13 +1,11 @@
 var express = require('express');
 var app = express();
 
-
+//Movido o carregamento de rotas para dentro do arquivo ./routers/index.js
+//assim é alterado somente um logar para adicionar novos arquivos de rota
+//conforme a aplicação vai crescendo
 var routers = require('./routers');
 app.use('/', routers);
-
-
-
-
 
 
 var server = app.listen(3000, function () {
