1c169e6d50e29e39b9aebe5ee5933e4e9b5e9eb4
Marcio de Almeida Rosa <marcio.almeida.rosa@gmail.com>
2017-05-24 22:27:36 -0300

    Explicando forma de carregar um módulo por require

--- BoasPraticas/app.js
+++ BoasPraticas/app.js
@@ -1,9 +1,14 @@
+//para usar módulo nativo ou instalado via npm, para usar somente "require" e o nome do módulo "express"
 var express = require('express');
 var app = express();
 
 //Movido o carregamento de rotas para dentro do arquivo ./routers/index.js
 //assim é alterado somente um logar para adicionar novos arquivos de rota
 //conforme a aplicação vai crescendo
+//para usar móduos que nós escrevemos usar "require" junto com "./" 
+//para não sei procurado de "node_modules do projeto ou da maquina"
+//sendo assim, será procurado por um arquivo chamado "routers.js" ou uma pasta "routers"
+//ou usar da seguinte forma './routers/index'
 var routers = require('./routers');
 app.use('/', routers);
 
