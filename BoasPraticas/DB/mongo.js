bf36baf42426908737ab47fb1e3df28727b4952d
Marcio De Almeida Rosa <devel02@GRMLT.LOCAL>
2017-05-25 17:07:10 -0300

    Alterações

--- BoasPraticas/DB/mongo.js
+++ BoasPraticas/DB/mongo.js
@@ -8,6 +8,9 @@ connectionString = 'mongodb://admin:n9mehu7HYWgG06YP@cluster0-shard-00-00-okd4p.
 //mLab
 connectionString = 'mongodb://admin:n9mehu7HYWgG06YP@ds155091.mlab.com:55091/primeiro-uso-node';
 
+//dynamodb AWS ???????????????????
+//arn:aws:dynamodb:us-east-2:954928321108:table/products
+
 var db = mongojs(connectionString, ['products']);
 
 //var db = mongojs('mongodb://admin:n9mehu7HYWgG06YP@cluster0-shard-00-00-okd4p.mongodb.net:27017,cluster0-shard-00-01-okd4p.mongodb.net:27017,cluster0-shard-00-02-okd4p.mongodb.net:27017/PRIMEIRO-USO-NODE?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin', ['products']);
